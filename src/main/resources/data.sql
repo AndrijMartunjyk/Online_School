cbb5295f75df75e23afbf689c031a2126a216fd5
AndrijMartunjyk <andrijmartunjyk@gmail.com>
2023-03-26 22:10:28 +0300

    41 homework

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,12 +24,13 @@ VALUES (1, 1),
        (3, 4),
        (3, 5);
 
-INSERT INTO online_school.teacher(first_name, last_name, email, phone_number, `role`, lecture_id)
-VALUES ('Ольга', 'Кличко', 'asd@gmail.com', 123456789, 'TEACHER', 1),
-       ('Іра', 'Тайсон', 'dasd@gmail.com', 123456789, 'TEACHER', 2),
-       ('Люда', 'Фюрі', 'wasd@gmail.com', 123456789, 'TEACHER', 3),
-       ('Надія', 'Усик', 'yasd@gmail.com', 123456789, 'TEACHER', 4),
-       ('Ольга', 'Алі', 'qasd@gmail.com', 123456789, 'TEACHER', 5);
+INSERT INTO online_school.teacher(teacher_id, first_name, last_name, email, phone_number, role, lecture_id, course_id)
+VALUES (1, 'Ольга', 'Кличко', 'asd@gmail.com', 123456789, 'TEACHER', 1, 1),
+       (2, 'Іра', 'Тайсон', 'dasd@gmail.com', 123456789, 'TEACHER', 2, 2),
+       (3, 'Люда', 'Фюрі', 'wasd@gmail.com', 123456789, 'TEACHER', 3, 3),
+       (4, 'Надія', 'Усик', 'yasd@gmail.com', 123456789, 'TEACHER', 4, 4),
+       (5, 'Ольга', 'Алі', 'qasd@gmail.com', 123456789, 'TEACHER', 5, 5);
+
 
 INSERT INTO online_school.lecture(lecture_name, `description`, teacher_id, course_id, creation_date, lecture_date)
 VALUES ('MySql', 'вивчення бази данних', 1, 1, '2020-1-23 12:00', '2022-5-29 12:00'),
@@ -37,25 +38,30 @@ VALUES ('MySql', 'вивчення бази данних', 1, 1, '2020-1-23 12:0
        ('method', 'вивчення методів', 3, 3, '2020-3-23 12:00', '2022-5-25 12:00'),
        ('Docker', 'вивчення docker', 4, 4, '2020-4-23 12:00', '2023-5-23 12:00'),
        ('Maven', 'Maven, встановлення', 5, 5, '2020-5-23 12:00', '2023-6-30 12:00');
+INSERT INTO online_school.lecture(lecture_name, `description`, teacher_id, course_id, creation_date, lecture_date)
+VALUES ('Junit', 'вивчення тестування', 3, 2, '2020-2-23 12:00', '2023-5-27 12:00'),
+       ('method', 'вивчення методів', 1, 3, '2020-3-23 12:00', '2022-5-25 12:00'),
+       ('Docker', 'вивчення docker', 3, 4, '2020-4-23 12:00', '2023-5-23 12:00');
 
-INSERT INTO online_school.additional_material(`name`, lecture_id, resource_type)
-VALUES ('url address', 1, 'URL'),
-       ('books', 2, 'BOOk'),
-       ('books', 2, 'BOOk'),
-       ('video materials', 1, 'VIDEO'),
-       ('books', 3, 'BOOk'),
-       ('video', 1, 'VIDEO'),
-       ('video', 1, 'VIDEO'),
-       ('video', 4, 'VIDEO'),
-       ('url address', 5, 'URL');
 
-INSERT INTO online_school.home_work(lecture_id, task)
-VALUES (4, 'study,study,study and study again !!!'),
-       (1, 'study,study,study and study again !!!'),
-       (5, 'study,study,study and study again !!!'),
-       (2, 'study,study,study and study again !!!'),
-       (3, 'study,study,study and study again !!!'),
-       (1, 'study,study,study and study again !!!'),
-       (5, 'study,study,study and study again !!!'),
-       (1, 'study,study,study and study again !!!');
+INSERT INTO online_school.additional_material(`name`, lecture_id, resource_type, course_id)
+VALUES ('url address', 1, 'URL', 1),
+       ('books', 2, 'BOOk', 3),
+       ('books', 2, 'BOOk', 4),
+       ('video materials', 1, 'VIDEO', 1),
+       ('books', 3, 'BOOk', 2),
+       ('video', 1, 'VIDEO', 4),
+       ('video', 1, 'VIDEO', 1),
+       ('video', 4, 'VIDEO', 5),
+       ('url address', 5, 'URL', 3);
+
+INSERT INTO online_school.home_work(lecture_id, task, course_id)
+VALUES (4, 'study,study,study and study again !!!', 2),
+       (1, 'study,study,study and study again !!!', 4),
+       (5, 'study,study,study and study again !!!', 3),
+       (2, 'study,study,study and study again !!!', 1),
+       (3, 'study,study,study and study again !!!', 5),
+       (1, 'study,study,study and study again !!!', 4),
+       (5, 'study,study,study and study again !!!', 3),
+       (1, 'study,study,study and study again !!!', 3);
 
--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -8,6 +8,7 @@ create table course
 );
 create table student
 (
+    PRIMARY KEY (`student_id`),
     student_id   INT AUTO_INCREMENT,
     first_name   VARCHAR(20),
     last_name    VARCHAR(40),
@@ -15,53 +16,56 @@ create table student
     phone_number VARCHAR(14),
     `role`       ENUM ('STUDENT'),
     lecture_id   LONG,
-    course_id    LONG,
-    PRIMARY KEY (`student_id`)
+    course_id    LONG
 );
 
 create table student_courses
 (
+    PRIMARY KEY (student_id, course_id),
     student_id INT NOT NULL,
     course_id  INT NOT NULL,
     FOREIGN KEY (student_id) REFERENCES student (student_id),
-    FOREIGN KEY (course_id) REFERENCES course (course_id),
-    PRIMARY KEY (student_id, course_id)
+    FOREIGN KEY (course_id) REFERENCES course (course_id)
 );
 create table teacher
 (
-    teacher_id   INT AUTO_INCREMENT,
+    teacher_key  INT PRIMARY KEY AUTO_INCREMENT,
+    teacher_id   VARCHAR(20) UNIQUE,
     first_name   VARCHAR(20),
     last_name    VARCHAR(40),
     email        VARCHAR(40) UNIQUE,
     phone_number VARCHAR(14),
     `role`       ENUM ('TEACHER'),
     lecture_id   LONG,
-    PRIMARY KEY (`teacher_id`)
+    course_id    LONG
 );
+
 create table lecture
 (
+    PRIMARY KEY (`lecture_id`),
     lecture_id    INT AUTO_INCREMENT,
     lecture_name  VARCHAR(20),
     `description` VARCHAR(40),
     teacher_id    LONG,
     course_id     LONG,
     creation_date DATETIME,
-    lecture_date  DATETIME,
-    PRIMARY KEY (`lecture_id`)
+    lecture_date  DATETIME
 );
 create table additional_Material
 (
+    PRIMARY KEY (`resource_id`),
     resource_id   INT AUTO_INCREMENT,
     `name`        VARCHAR(50),
     lecture_id    LONG,
     resource_type ENUM ('URL','BOOk','VIDEO'),
-    PRIMARY KEY (`resource_id`)
+    course_id     LONG
 );
 create table home_work
 (
+    PRIMARY KEY (`homework_id`),
     homework_id INT AUTO_INCREMENT,
     lecture_id  LONG,
     task        VARCHAR(400),
-    PRIMARY KEY (`homework_id`)
+    course_id   LONG
 );
 
